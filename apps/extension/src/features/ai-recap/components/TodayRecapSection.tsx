958b591cac91516628c1a69d4596ef5f28535ed7
suminhan123 <suminh324@gmail.com>
2026-02-25 23:10:56 +0900

    feat: recap topic 스타일 위치 조정

--- apps/extension/src/features/ai-recap/components/TodayRecapSection.tsx
+++ apps/extension/src/features/ai-recap/components/TodayRecapSection.tsx
@@ -20,24 +20,26 @@ const TodayRecapSection = ({
         <p className="text-subtitle-2-rg text-gray-800">Today&apos;s Recap</p>
         <h2 className="text-headline-sb text-gray-900">{title ?? "-"}</h2>
       </div>
-      <div className="mt-4 rounded-[0.75rem] bg-gray-75 flex flex-col">
-        <img
-          src={imageUrl ?? RecapImg}
-          className="h-auto rounded-t-[0.75rem] w-full max-w-xs object-contain"
-        />
-        <div className="flex items-center">
-          <div className="flex flex-1 flex-col py-3 pl-4 gap-1">
-            <p className="text-label-2 text-gray-500">총 스크린타임</p>
-            <p className="text-body-2 text-gray-900">
-              {formatDuration(totalScreenTime ?? 0)}
-            </p>
-          </div>
-          <Divider className="w-0.5 h-12 mx-4" />
-          <div className="flex flex-1 flex-col py-3 pl-4 gap-1">
-            <p className="text-label-2 text-gray-500">측정시간</p>
-            <p className="text-body-2 text-gray-900">
-              {formatTimeRange(startedAt, closedAt) ?? "-"}
-            </p>
+      <div className="flex justify-center mt-4">
+        <div className="flex flex-col w-full max-w-md max-h-[600px]">
+          <img
+            src={imageUrl ?? RecapImg}
+            className="h-auto rounded-t-[0.75rem] w-full object-contain"
+          />
+          <div className="flex items-center w-full bg-gray-75 rounded-b-[0.75rem]">
+            <div className="flex flex-1 flex-col py-3 pl-4 gap-1">
+              <p className="text-label-2 text-gray-500">총 스크린타임</p>
+              <p className="text-body-2 text-gray-900">
+                {formatDuration(totalScreenTime ?? 0)}
+              </p>
+            </div>
+            <Divider className="w-0.5 h-12 mx-4" />
+            <div className="flex flex-1 flex-col py-3 pl-4 gap-1">
+              <p className="text-label-2 text-gray-500">측정시간</p>
+              <p className="text-body-2 text-gray-900">
+                {formatTimeRange(startedAt, closedAt) ?? "-"}
+              </p>
+            </div>
           </div>
         </div>
       </div>
