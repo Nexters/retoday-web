9bbe3c47af9ac943ed41aec062053122397c32c4
suminhan123 <suminh324@gmail.com>
2026-02-25 22:36:52 +0900

    feat: api recap get api 연결

--- apps/extension/src/features/ai-recap/components/TodayRecapDetail.tsx
+++ apps/extension/src/features/ai-recap/components/TodayRecapDetail.tsx
@@ -1,31 +1,23 @@
 import Divider from "@/components/Divider";
+import type { AiRecapSection } from "@/entities/ai-recap/model/ai-recap.type";
 
-const TodayRecapDetail = () => {
+const TodayRecapDetail = ({ sections }: { sections: AiRecapSection[] }) => {
   return (
     <>
-      <div className="pt-4 px-6 pb-6">
-        <p className="text-subtitle-2-sb text-gray-500">01</p>
-        <p className="text-subtitle-2-sb mt-1 text-gray-900">
-          개발하며 고군분투한 하루
-        </p>
-        <p className="text-body-2 mt-4 text-gray-900">
-          오늘은 주로 개발과 학습에 집중하셨네요. 오전 10시부터 오후 3시까지
-          가장 활발한 활동을 보였으며, 오늘은 주로 개발과 학습에 집중하셨네요.
-        </p>
-      </div>
-
-      <Divider className="h-[0.0625rem] w-full" />
-
-      <div className="pt-4 px-6 pb-6">
-        <p className="text-subtitle-2-sb text-gray-500">02</p>
-        <p className="text-subtitle-2-sb mt-1 text-gray-900">
-          업무 효율화를 위한 여정
-        </p>
-        <p className="text-body-2 mt-4 text-gray-900">
-          AI와 피그마를 연결하거나, 디자인 시스템 정비를 위한 MCP(Model Context
-          Protocol) 활용법 등 최신 AI 툴을 업무에 녹여내려 노력했어요.
-        </p>
-      </div>
+      {sections.map((section, index) => (
+        <>
+          <div key={index} className="pt-4 px-6 pb-6">
+            <p className="text-subtitle-2-sb text-gray-500">{index + 1}</p>
+            <p className="text-subtitle-2-sb mt-1 text-gray-900">
+              {section.title}
+            </p>
+            <p className="text-body-2 mt-4 text-gray-900">{section.content}</p>
+          </div>
+          {index < sections.length - 1 && (
+            <Divider className="h-[0.0625rem] w-full" />
+          )}
+        </>
+      ))}
     </>
   );
 };
--- apps/extension/src/features/ai-recap/components/TodayRecapSection.tsx
+++ apps/extension/src/features/ai-recap/components/TodayRecapSection.tsx
@@ -1,23 +1,43 @@
+import RecapImg from "@/assets/imgs/recap-img.png";
 import Divider from "@/components/Divider";
+import type { AiRecapResponse } from "@/entities/ai-recap/model/ai-recap.type";
+import { formatDuration, formatTimeRange } from "@/utils/date";
 
-const TodayRecapSection = () => {
+const TodayRecapSection = ({
+  title,
+  imageUrl,
+  startedAt,
+  closedAt,
+  timelines,
+}: Partial<AiRecapResponse>) => {
+  const totalScreenTime = timelines?.reduce(
+    (acc, curr) => acc + curr.durationMinutes * 60,
+    0,
+  );
   return (
     <div className="bg-white pt-4 px-5">
       <div className="space-y-2">
-        <p className="text-subtitle-2-rg text-gray-800">Today’s Recap</p>
-        <h2 className="text-headline-sb text-gray-900">집중적인 연구의 하루</h2>
+        <p className="text-subtitle-2-rg text-gray-800">Today&apos;s Recap</p>
+        <h2 className="text-headline-sb text-gray-900">{title ?? "-"}</h2>
       </div>
       <div className="mt-4 rounded-[0.75rem] bg-gray-75 flex flex-col">
-        <div className="w-full h-36 bg-blue-50 rounded-t-[0.75rem]" />
+        <img
+          src={imageUrl ?? RecapImg}
+          className="h-auto rounded-t-[0.75rem] w-full max-w-xs object-contain"
+        />
         <div className="flex items-center">
           <div className="flex flex-1 flex-col py-3 pl-4 gap-1">
             <p className="text-label-2 text-gray-500">총 스크린타임</p>
-            <p className="text-body-2 text-gray-900">5시간 35분</p>
+            <p className="text-body-2 text-gray-900">
+              {formatDuration(totalScreenTime ?? 0)}
+            </p>
           </div>
           <Divider className="w-0.5 h-12 mx-4" />
           <div className="flex flex-1 flex-col py-3 pl-4 gap-1">
             <p className="text-label-2 text-gray-500">측정시간</p>
-            <p className="text-body-2 text-gray-900">08am - 12pm</p>
+            <p className="text-body-2 text-gray-900">
+              {formatTimeRange(startedAt, closedAt) ?? "-"}
+            </p>
           </div>
         </div>
       </div>
