b47468831e9a4c640981ee08a0a6b2b2ec6e85ab
suminhan123 <suminh324@gmail.com>
2026-05-24 19:34:26 +0900

    feat(ui): 공통 stack ui 컴포넌트 추가

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,13 +2,21 @@ export * from "./accordion";
 export * from "./badge";
 export * from "./button";
 export * from "./date-picker";
+export * from "./flex";
 export * from "./input";
 export * from "./select";
 export * from "./skeleton";
+export * from "./stack";
 export * from "./switch";
 export * from "./tabs";
 export * from "./toggle-group";
 export * from "./tooltip";
 export * from "./utils/cn";
 export * from "./utils/date";
+export {
+  GAP_SEMANTIC,
+  GAP_X_SEMANTIC,
+  GAP_Y_SEMANTIC,
+  type GapSize,
+} from "./utils/gap-scale";
 export * from "./weekly-bar-chart";
