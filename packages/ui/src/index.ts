05b333eeb3f2d2b3e1ef80721f0c380f845c2ea0
suminhan123 <suminh324@gmail.com>
2026-05-24 19:37:16 +0900

    feat(ui): 공통 divider ui 컴포넌트 추가

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,6 +2,7 @@ export * from "./accordion";
 export * from "./badge";
 export * from "./button";
 export * from "./date-picker";
+export * from "./divider";
 export * from "./flex";
 export * from "./input";
 export * from "./item";
