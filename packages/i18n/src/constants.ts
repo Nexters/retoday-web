eac2f01a56e0a3d0d61e1a7f6dc9a23ba2a40680
suminhan123 <suminh324@gmail.com>
2026-06-21 16:01:46 +0900

    feat(i18n): 일본어 옵션 주석 처리

--- packages/i18n/src/constants.ts
+++ packages/i18n/src/constants.ts
@@ -17,7 +17,7 @@ import koSettings from "./locales/ko/setting.json";
 export const LanguageList = {
   EN: "en",
   KO: "ko",
-  JA: "ja",
+  // JA: "ja",
 } as const;
 
 export type LanguageType = (typeof LanguageList)[keyof typeof LanguageList];
@@ -69,11 +69,11 @@ export type Resources = (typeof resources)[keyof typeof resources];
 export const LANGUAGE_LABEL: Record<LanguageType, string> = {
   ko: "한국어",
   en: "English",
-  ja: "日本語",
+  // ja: "日本語",
 };
 
 export const LANGUAGE_UTC_OFFSET: Record<LanguageType, string> = {
   en: "UTC-08:00",
   ko: "UTC+09:00",
-  ja: "UTC+09:00",
+  // ja: "UTC+09:00",
 };
