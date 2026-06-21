3b388131cc30337f97243cb357ad104a9867ab03
suminhan123 <suminh324@gmail.com>
2026-06-21 20:47:44 +0900

    chore: extension 빌드 환경 변수 추가

--- apps/extension/src/app/types/vite-env.d.ts
+++ apps/extension/src/app/types/vite-env.d.ts
@@ -1,5 +1,17 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_BACKEND_URL: string;
+  readonly VITE_GOOGLE_CLIENT_ID: string;
+  readonly VITE_EXTENSION_KEY: string;
+  readonly VITE_GA_MEASUREMENT_ID: string;
+  readonly VITE_GA_API_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare module "*.svg" {
   const content: string;
   export default content;
