ccc95de06b023bfd122bfd0973fb5d1f56df66e6
suminhan123 <suminh324@gmail.com>
2026-06-21 14:27:28 +0900

    feat(api): 타임존 및 언어 enum 추가

--- packages/lib/src/dayjs/time-zone.const.ts
+++ packages/lib/src/dayjs/time-zone.const.ts
@@ -4,3 +4,13 @@ export const CURRENT_TIMEZONE =
   typeof Intl === "undefined"
     ? DEFAULT_TIME_ZONE
     : Intl.DateTimeFormat().resolvedOptions().timeZone || DEFAULT_TIME_ZONE;
+
+export enum SERVER_TIMEZONE {
+  SEOUL = "SEOUL",
+  UTC = "UTC",
+}
+
+export const SERVER_CURRENT_TIMEZONE =
+  CURRENT_TIMEZONE === "Asia/Seoul"
+    ? SERVER_TIMEZONE.SEOUL
+    : SERVER_TIMEZONE.UTC;
---
+++ packages/lib/src/language.ts
@@ -0,0 +1,5 @@
+export enum Language {
+  KOREA = "KOREAN",
+  ENGLISH = "ENGLISH",
+  // JAPANESE = "JAPANESE",
+}
